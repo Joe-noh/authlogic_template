5e62abe3d1d8f3ed3ea40b0cde597a0bc5ddda23
Joe-noh <goflb.jh@gmail.com>
2013-09-27 19:59:18 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthlogicTemplate::Application.config.secret_key_base = 'a909e1475c3e7ba6a920d76c063d20984dda89e406e6952075622182b4c2d720177fc69bb069fb4298a3e4c464ce6e910d1cb3d260703748b5885772aee9e336'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthlogicTemplate::Application.config.session_store :cookie_store, key: '_authlogic_template_session'
